f31b0d7d7ff2d5e6ac5ea7ab55fb1e97b33dd1ba
Jonny Marshall <jonny@Jonnys-MBP.cust.communityfibre.co.uk>
2020-09-24 17:55:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { clientId } from "./secrets";
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import { GoogleLogin } from "react-google-login";
+import "./App.css";
 
 function App() {
+  const googlePeopleUrl =
+    "https://people.googleapis.com/v1/people/me/connections?personFields=names,emailAddresses";
+
+  const getData = async (url, accessToken) => {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+  };
+
+  const responseGoogle = ({ accessToken }) => {
+    getData(googlePeopleUrl, accessToken)
+      .then((data) => {
+        console.log(data); // JSON data parsed by `data.json()` call
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{clientId}</p>
+      <GoogleLogin
+        clientId="952301080115-odt17vabnr32itq23pir8357fdd81e9k.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
     </div>
   );
 }
 
 export default App;
+// Client ID
+// 762892394508-23fcllqa715aj6cqd7egf71r8sm3pp88.apps.googleusercontent.com
+
+// Client Secret
+// xZL5DJRFbmniMRpQbQVfkwbq
+
+// API Key AIzaSyBCq7FDBc8DsI7jLfI5p21FKlntS7gVHfo
